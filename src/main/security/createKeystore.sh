cb515820dfdd1192a62d9e29dae559987caf5536
mbarragan <mbarragan@10.59.1.40>
2019-03-11 15:37:26 -0600

    Alias

--- src/main/security/createKeystore.sh
+++ src/main/security/createKeystore.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+export CLIENT=cliente
 export PRIVATE_KEY_FILE=pri_key.pem
 export CERTIFICATE_FILE=certificate.pem
-export SUBJECT=/C=/ST=/L=/O=/CN=cliente
+export SUBJECT="/C=/ST=/L=/O=/CN=${CLIENT}"
 export PKCS12_FILE=keypair.p12
 export KEYSTORE_PASSWORD=
 export KEY_PASSWORD=
 export KEYSTORE_FILE=keystore.jks
-export ALIAS=cliente
+export ALIAS=${CLIENTE}
 #Borra todos los archivos
 rm -rf ${KEYSTORE_FILE}
 rm -rf ${PRIVATE_KEY_FILE}
@@ -16,7 +17,7 @@ rm -rf ${PKCS12_FILE}
 openssl ecparam -name secp384r1 -genkey -out ${PRIVATE_KEY_FILE}
 openssl req -new -x509 -days 365 -key ${PRIVATE_KEY_FILE} -out ${CERTIFICATE_FILE} -subj "${SUBJECT}"
 #Genera el archivo pkcs12 a partir de la llave privada y el certificado.
-openssl pkcs12 -name kobra -export -out ${PKCS12_FILE} -inkey ${PRIVATE_KEY_FILE} -in ${CERTIFICATE_FILE} -password pass:${KEY_PASSWORD}
+openssl pkcs12 -name ${ALIAS} -export -out ${PKCS12_FILE} -inkey ${PRIVATE_KEY_FILE} -in ${CERTIFICATE_FILE} -password pass:${KEY_PASSWORD}
 #Genera un keystore con un par de llaves dummy.
 keytool -genkey -alias dummy -keyalg RSA -keysize 2048 -keystore ${KEYSTORE_FILE} -dname "CN=dummy, OU=, O=, L=, S=, C=" -storepass ${KEYSTORE_PASSWORD} -keypass ${KEY_PASSWORD}
 #Lista el contenido del kesystore.
