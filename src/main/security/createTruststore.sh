5f9d6f54029f66916b34ae9d797c11c37b532a61
mbarragan <mbarragan@10.59.1.40>
2019-03-11 16:03:35 -0600

    alias

--- src/main/security/createTruststore.sh
+++ src/main/security/createTruststore.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-export THIRD_PART_CERTIFICATE=cdc_certificate.pem
+export CLIENT=
+export THIRD_PART_CERTIFICATE=
 export TRUSTSTORE_FILE=truststore.jks
 export TRUSTSTORE_PASSWORD=
 export KEY_PASSWORD=
-export CERT_ALIAS=cliente
+export ALIAS=${CLIENT}
 #Genera un truststore con un par de llaves dummy.
 keytool -genkey -alias dummy -keyalg RSA -keysize 2048 -keystore ${TRUSTSTORE_FILE} -dname "CN=dummy, OU=, O=, L=, S=, C=" -storepass ${TRUSTSTORE_PASSWORD} -keypass ${KEY_PASSWORD}
 #Lista el contenido del truststore.
@@ -11,7 +12,7 @@ keytool -list -keystore ${TRUSTSTORE_FILE} -storepass ${TRUSTSTORE_PASSWORD}
 #Elimina el par de llaves dummy.
 keytool -delete -alias dummy -keystore ${TRUSTSTORE_FILE} -storepass ${TRUSTSTORE_PASSWORD}
 #Agrega el certificado al truststore.
-keytool -import -file ${THIRD_PART_CERTIFICATE} -keystore ${TRUSTSTORE_FILE} -storepass ${TRUSTSTORE_PASSWORD} -noprompt -alias ${CERT_ALIAS}
+keytool -import -file ${THIRD_PART_CERTIFICATE} -keystore ${TRUSTSTORE_FILE} -storepass ${TRUSTSTORE_PASSWORD} -noprompt -alias ${ALIAS}
 #Lista el contenido del truststore.
 keytool -list -keystore ${TRUSTSTORE_FILE} -storepass ${TRUSTSTORE_PASSWORD}
 #Borra todos los archivos excepto el truststore.
